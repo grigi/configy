c3e4f520e329b34e6dfb5cbd8e6ee7165cce42b5
Grigi <nagrigoriadis@gmail.com>
2015-09-08 10:21:19 +0200

    Remove unnesecary __all__ since imports are carefully picked

--- configy/__init__.py
+++ configy/__init__.py
@@ -5,5 +5,4 @@ from . import testconfig
 from .config_container import config, load_config, ConfigyError
 from .helpers import to_bool
 
-__all__ = ['config', 'load_config', 'testconfig']
 __version__ = '0.1.0'
