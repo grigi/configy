ea1b389c938602b9f0823ab710950e19152b1c63
Nickolas Grigoriadis <nagrigoriadis@gmail.com>
2025-04-21 15:07:13 +0100

    Cleanup base import

--- configy/__init__.py
+++ configy/__init__.py
@@ -1,8 +1,8 @@
 '''
 Simple Configuration manager, plays well with testing
 '''
-from . import testconfig
 from .config_container import config, load_config, ConfigyError
 from .helpers import to_bool
 
 __version__ = '0.1.4'
+__all__ = ('config', 'load_config', 'ConfigyError', 'to_bool')
